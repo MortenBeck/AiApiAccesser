01a0ad251594c07f6f8dc0fbc7f158bb47c34b16
MortenBeck <s224572@dtu.dk>
2025-05-14 18:26:07 +0200

    Initial Commit

---
+++ AiApiAccesser/AiApiAccesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiApiAccesserApp.swift
+//  AiApiAccesser
+//
+//  Created by Morten on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AiApiAccesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
